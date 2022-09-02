84ae02b23956fe531c4fb367d1c26ad1a78c259a
MasterX33 <masterxroot@gmail.com>
2022-09-02 20:06:56 +0300

    3-rd add lib Faker and change function

--- main.py
+++ main.py
@@ -3,6 +3,10 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+from faker import Faker
+
+fake = Faker()
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +15,8 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Anton')
+    print_hi(fake.name())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
