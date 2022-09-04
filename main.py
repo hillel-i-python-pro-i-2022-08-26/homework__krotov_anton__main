09dc34ab40d374c4fe426a3f3bf4b6221dab79a6
MasterX33 <masterxroot@gmail.com>
2022-09-04 14:32:53 +0300

    Game words Playrs name input

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
-# This is a sample Python script.
+#Game WORDS
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-from faker import Faker
-
-fake = Faker()
+igroki = []
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def player():
 
+    players = input()
+    while players != "Start":
+         print(player())
+         igroki.append(players)
+         print(igroki)
+    else:
+        print(igroki)
+        print("Поехали")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi(fake.name())
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-
+player()
